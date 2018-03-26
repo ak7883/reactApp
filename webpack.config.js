438474d5c1ed3b7a6ebb6a5a186d9e2730e40899
Ajay Kumar <ak7883@gmail.com>
2018-03-26 18:21:31 +0530

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var config = {
+   entry: './App/Main.js',
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+module.exports = config;
